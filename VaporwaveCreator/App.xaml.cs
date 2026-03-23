b4685578b7994bb12e9ab47a691882824b4b3c6c
Micilini Roll <micilini@hotmail.com>
2026-03-23 12:56:30 -0300

    feat: mutex for single run app

--- VaporwaveCreator/App.xaml.cs
+++ VaporwaveCreator/App.xaml.cs
@@ -1,14 +1,37 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
+using System.Threading;
 using System.Windows;
 
 namespace VaporwaveCreator
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private static Mutex _mutex = null;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string mutexName = "VaporwaveCreator_SingleInstance_Mutex";
+
+            _mutex = new Mutex(true, mutexName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_mutex != null)
+            {
+                _mutex.ReleaseMutex();
+                _mutex.Dispose();
+            }
+
+            base.OnExit(e);
+        }
+    }
 }
